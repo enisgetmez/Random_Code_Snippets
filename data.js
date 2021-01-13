eb17aeba4cd17f62545375c03ef7938cb5a55f77
Enis Getmez <mentalistler@mit.tc>
2021-01-14 00:57:37 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,43 @@
+const python_kodlar = [
+	{
+      "kod_resmi": "https://hackers.istanbul/random_kod/resimler/adres%20ve%20koordinat%20bulma.png",
+      "aciklama": "Python ile Adres ve koordinat sorgulama",
+      "link": "https://hackers.istanbul/random_kod/resimler/adres%20ve%20koordinat%20bulma.py",
+    },
+    {
+      "kod_resmi": "https://hackers.istanbul/random_kod/resimler/arduino_kontrol.png",
+      "aciklama": "Python ile arduino'da ışık açma/kapatma",
+      "link": "https://hackers.istanbul/random_kod/resimler/arduino_kontrol.py",
+    },
+    {
+      "kod_resmi": "https://hackers.istanbul/random_kod/resimler/mysql ile baglanti kurmak.png",
+      "aciklama": "Python'da'Mysql ile bağlantı kurmak",
+      "link": "https://hackers.istanbul/random_kod/resimler/mysql ile baglanti kurmak.py", 
+    },
+    {
+      "kod_resmi": "https://hackers.istanbul/random_kod/resimler/sahte_resim_indirme.png",
+      "aciklama": "thispersondoesnotexist üzerinden rastgele gerçek olmayan insan fotoğrafları indirmek",
+      "link": "https://hackers.istanbul/random_kod/resimler/sahte_resim_indirme.py",
+    },
+    {
+      "kod_resmi": "https://hackers.istanbul/random_kod/resimler/speech_recognition.png",
+      "aciklama": "Python ile sesi yazıya çevirme", 
+      "link": "https://hackers.istanbul/random_kod/resimler/speech_recognition.py",
+    },
+    {
+      "kod_resmi": "https://hackers.istanbul/random_kod/resimler/fizik_simulasyon.png",
+      "aciklama": "Python ile fizik simülasyonu geliştirmek.",
+      "link": "https://hackers.istanbul/random_kod/resimler/fizik_simulasyon.py",
+    },
+    {
+      "kod_resmi": "https://hackers.istanbul/random_kod/resimler/internet_baglantisi_kontrol.png",
+      "aciklama": "Python ile internet bağlantısını kontrol etmek.",
+      "link": "https://hackers.istanbul/random_kod/resimler/internet_baglantisi_kontrol.py",
+    },
+    {
+      "kod_resmi": "https://hackers.istanbul/random_kod/resimler/kripto_para_cuzdan.png",
+      "aciklama": "Python ile kripto para cüzdanı oluşturmak.",
+      "link": "https://hackers.istanbul/random_kod/resimler/kripto_para_cuzdan.py",
+    },
+
+];
---
+++ main.js
@@ -0,0 +1,27 @@
+﻿var link;
+function randomkod() {
+    let random_sec = python_kodlar[Math.floor(Math.random() * python_kodlar.length)];
+    let resim = document.getElementById("kod");
+    let bilgi = document.getElementById("bilgi");
+    let img = document.createElement("img");
+    img.src = random_sec.kod_resmi;
+    console.log(resim.children.length);
+
+    if(resim.children.length>0) 
+        {
+            resim.removeChild(resim.firstChild);
+        }
+        
+    resim.appendChild(img);
+    bilgi.innerHTML = random_sec.aciklama;
+    link = random_sec.link;
+}
+function indir()
+{
+    if(link!=undefined){
+     location.href = link;
+    }
+    else{
+        alert("Resim indirebilmek için rastgele kod oluşturun.")
+    }
+}
